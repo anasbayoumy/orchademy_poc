8113c832a375498b8e083af1800e06022da31506
Anas <anas@Anass-MacBook-Air.local>
2026-01-18 21:38:55 +0200

    Fix: GH workflow

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enable static export for GitHub Pages
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  // Optional: Set basePath if your repo name is not the root
-  // basePath: '/orchademy_poc',
-  // trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
